77536ee93ab695334d64c748c86c9a70f9d1331e
mgaleski <mpgaleski@outlook.com>
2020-09-14 23:06:05 -0500

    requirements

--- model.py
+++ model.py
@@ -56,4 +56,6 @@ pickle.dump(svc, open('model.pkl', 'wb'))
 
 import nltk
 import sklearn
+import pandas
 print('The scikit-learn version is {}.'.format(sklearn.__version__))
+print('The pandas version is {}.'.format(pandas.__version__))
